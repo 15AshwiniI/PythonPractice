a649170fabd456c3ed36ecab4e625baf668e309c
Ashwini Iyer <bookworm.iyer@gmail.com>
2015-10-19 13:33:12 -0400

    Worked with getting and changing user input

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,22 @@
-print "2+2 = 4"
+print "Calculator"
+
+#addition
 x = 1
-if x == 1:
-    print "x = 1 "
+y = 2
+z = x + y
+print z
+
+print "Enter a word"
+s = raw_input()
+print s
+
+print "Enter a word"
+m = raw_input()
+print s + " " + m
+
+print "Enter a number"
+m = input()
+print "Enter another number"
+n = input()
+#print "Product of " + m + " and " + n
+print m * n
