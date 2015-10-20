27a9571e7b9b916f6bc195a7c01a7b3564f2cb7c
Ashwini Iyer <bookworm.iyer@gmail.com>
2015-10-20 11:35:08 -0400

    Added file spell.words

--- SpellChecker.py
+++ SpellChecker.py
@@ -1,1 +1,9 @@
+# used tutorial on: http://www.openbookproject.net/py4fun/spellCheck/index.html
 print "Welcome to SpellChecker"
+
+#opens and reads from the spell.words file. It has over 50,000 words and serves as our dictionary
+words = open("spell.words.txt").readlines()
+words = map(lambda x: x.strip(), words)
+print "a" in words
+for i in range(1000) : a = 'zygotic' in words
+print "completed"
